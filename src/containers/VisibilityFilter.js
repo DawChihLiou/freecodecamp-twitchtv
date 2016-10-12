47791ab9154c79d2e80b98c49063c08c1d0d29a2
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-10-12 22:16:58 +0800

    add VisibilityFilter container

--- src/containers/VisibilityFilter.js
+++ src/containers/VisibilityFilter.js
@@ -1,3 +1,24 @@
 import { connect } from 'react-redux'
 import { setVisibility } from '../actions'
 import Filter from '../components/Filter'
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibility
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibility(ownProps.filter))
+    }
+  }
+}
+
+const VisibilityFilter = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Filter)
+
+export default VisibilityFilter
