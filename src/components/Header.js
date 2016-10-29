70ff344818476573d68e1f8ea3342b3b90912791
Daw-Chih Liou <dawochih.liou@gmail.com>
2016-10-29 12:22:23 +0800

    add Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-xs-12 text-center">
+        <h1>Free Code Camp Zipline Twitchtv Viewer</h1>
+      </div>
+    </div>
+  </div>
+)
+
+export default Header
--- src/containers/TwitchViewer.js
+++ src/containers/TwitchViewer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import VisibleChannelList from './VisibleChannelList'
 import FilterList from '../components/FilterList'
+import Header from '../components/Header'
 import '../App.css'
 
 const TwitchViewer = () => {
   return (
     <div className="container">
+      <Header />
       <FilterList />
       <VisibleChannelList />
     </div>
